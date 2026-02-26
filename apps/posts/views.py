db83d6559db30763d8959f52e5e94cb4b137ed7c
SabuThess <dilan.developer22@gmail.com>
2026-02-26 15:52:09 -0500

    Add posts views

--- apps/posts/views.py
+++ apps/posts/views.py
@@ -1,18 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import PostModel
 from .forms import PostForm
 
-# Create your views here.
+def home(request): 
+    posts = PostModel.objects.all()
+    return render(request, 'posts/home.html', {'posts': posts})
+
 def create_post(request):
     form = PostForm()
-    return render(request, 'create_posts.html', {'form': form})
+    if(request.method == 'POST'):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    return render(request, 'posts/create_post.html', {'form': form})
+
+    
+
+def view_post(request, id):
+    post = PostModel.objects.get(id=id)
+    return render(request, 'posts/post_view.html', {'post': post})
 
 
-def update_post(request):
-    return 
 
-def delete_post(request):
-    return 
+def update_post(request, id):
+    post = PostModel.objects.get(id=id)
+    form = PostForm(instance=post)
+    if(request.method == 'POST'):
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect(f'post_view', id=post.id)
+    return render(request, 'posts/update_post.html', {'form': form})
 
-def view_post(request):
-    return 
+
+
+def delete_post(request, id):
+    post = PostModel.objects.get(id=id)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+    
+    return render(request, 'posts/delete_post.html', {'post': post})
+
 
