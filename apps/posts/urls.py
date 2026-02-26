bc8bc33d63a0accb4fc31e607a35dd06cc3aa818
SabuThess <dilan.developer22@gmail.com>
2026-02-26 15:51:57 -0500

    Add posts form

--- apps/posts/urls.py
+++ apps/posts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path 
-from . import views
+from .views import home, create_post, view_post, update_post, delete_post
 
 urlpatterns = [
-    path('create_post/', views.create_post,name="create_post"),
-    path('update_post/<int:id>', views.update_post ,name="update_post" ),
-    path('delete_post/<int:id>', views.delete_post,name="delete_post"),
-    path('view_post', views.view_post, name="post_view"),
+    path('', home , name="home"),
+    path('create_post/', create_post, name="create_post"),
+    path('posts/<int:id>', view_post, name="post_view"),
+    path('update_post/<int:id>', update_post ,name="update_post" ),
+    path('delete_post/<int:id>', delete_post,name="delete_post")
 ]
