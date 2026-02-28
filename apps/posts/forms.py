765e95036267f9f589ce15d12b4e3533e2eeb03d
SabuThess <dilan.developer22@gmail.com>
2026-02-28 18:35:01 -0500

    Add title and content as widget

--- apps/posts/forms.py
+++ apps/posts/forms.py
@@ -1,9 +1,18 @@
-from django import forms 
+from django import forms
 from .models import PostModel
 
 class PostForm(forms.ModelForm):
-    class Meta: 
+    class Meta:
         model = PostModel
-        fields=['title', 'content']
+        fields = ['title', 'content']
 
- 
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-teal-400',
+                'placeholder': 'Post title'
+            }),
+            'content': forms.Textarea(attrs={
+                'class': 'w-full p-2 border border-gray-300 rounded h-32 focus:outline-none focus:ring-2 focus:ring-teal-400',
+                'placeholder': 'Write your content here..!'
+            }),
+        }
